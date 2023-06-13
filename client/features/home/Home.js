1cb70bf4551396b27fb51cfb42aeb6fb38e98df6
JoeRod22 <usmarine0917@yahoo.com>
2023-06-13 17:52:11 -0400

    updated home.js

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import Footer from "../footer/footer";
+// import Footer from "../footer/footer";
 
 /**
  * COMPONENT
