61c0a42b917271484e35e8cd44210ff4c67a1246
Dylan <dylansy123@gmail.com>
2023-06-14 10:26:08 -0500

    file convention change

--- client/features/products/Products.js
+++ client/features/products/Products.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchAllProducts, selectProducts } from "../../app/productSlice.js";
+import { fetchAllProducts, selectProducts } from "./productSlice.js";
 
 const Products = () => {
   const dispatch = useDispatch();
